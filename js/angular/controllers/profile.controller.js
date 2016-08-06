70e2ca1a49958ad26f5cd7cf3534c079b8da0f96
Obadh Koujan <obadhk@live.co.uk>
2016-08-06 16:35:37 +0200

    First Upload

---
+++ js/angular/controllers/profile.controller.js
@@ -0,0 +1,22 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('profile')
+        .controller("profileController", profileController);
+
+    profileController.$inject = ['$scope'];
+
+    function profileController($scope) {
+        $scope.profile = {
+            firstName: "",
+            lastName: "",
+            nickname: "",
+            city: "",
+            phone: ""
+        };
+
+        $scope.editProfile = true;
+    }
+
+})();
---
+++ js/angular/profile.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular.module('profile', [
+        'profile.directives'
+    ]);
+
+    angular.module('profile.directives', []);
+})();
